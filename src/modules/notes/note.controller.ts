bf36f1ddb2748787d8d10714e8fb5f229e98d1c0
kurushiba <156669290+kurushiba@users.noreply.github.com>
2025-10-27 09:15:53 +0900

    Update note.controller.ts

--- src/modules/notes/note.controller.ts
+++ src/modules/notes/note.controller.ts
@@ -16,7 +16,7 @@ noteController.get('/', Auth, async (req: Request, res: Response) => {
   try {
     const query = (req.query.q as string) || '';
     const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 20;
+    const limit = parseInt(req.query.limit as string) || 12;
     const skip = (page - 1) * limit;
 
     const queryBuilder = noteRepository
